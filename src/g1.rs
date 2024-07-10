31fb3d91494b29e2410c768d792f488fbb9ad59a
Andrew Whitehead <cywolf@gmail.com>
2024-07-10 10:54:11 -0700

    add fast multi-exponentiation for affine g1, g2
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- src/g1.rs
+++ src/g1.rs
@@ -7,6 +7,7 @@ use core::{
     borrow::Borrow,
     fmt,
     iter::Sum,
+    mem::transmute,
     ops::{Add, AddAssign, Mul, MulAssign, Neg, Sub, SubAssign},
 };
 use elliptic_curve::consts::U48;
@@ -529,6 +530,16 @@ impl G1Affine {
                 "not on curve",
             ))
         }
+    }
+
+    pub fn sum_of_products(points: &[Self], scalars: &[Scalar]) -> G1Projective {
+        let num_components = points.len().min(scalars.len());
+        let mut raw_scalars = Vec::with_capacity(num_components * 32);
+        for s in scalars {
+            raw_scalars.extend_from_slice(&s.to_le_bytes());
+        }
+        let inner_points = unsafe { transmute::<_, &[blst_p1_affine]>(points) };
+        G1Projective(inner_points.mult(&raw_scalars, 255))
     }
 }
 
@@ -1638,6 +1649,29 @@ mod tests {
         }
 
         let pippenger = G1Projective::sum_of_products(points.as_slice(), scalars.as_slice());
+
+        assert_eq!(naive, pippenger);
+    }
+
+    #[test]
+    fn test_multi_exp_affine() {
+        const SIZE: usize = 10;
+        let mut rng = XorShiftRng::from_seed([
+            0x59, 0x62, 0xbe, 0x5d, 0x76, 0x3d, 0x31, 0x8d, 0x17, 0xdb, 0x37, 0x32, 0x54, 0x06,
+            0xbc, 0xe5,
+        ]);
+
+        let points: Vec<G1Affine> = (0..SIZE)
+            .map(|_| G1Projective::random(&mut rng).to_affine())
+            .collect();
+        let scalars: Vec<Scalar> = (0..SIZE).map(|_| Scalar::random(&mut rng)).collect();
+
+        let mut naive = points[0] * scalars[0];
+        for i in 1..SIZE {
+            naive += points[i] * scalars[i];
+        }
+
+        let pippenger = G1Affine::sum_of_products(points.as_slice(), scalars.as_slice());
 
         assert_eq!(naive, pippenger);
     }
